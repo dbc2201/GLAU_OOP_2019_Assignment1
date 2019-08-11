f70e73e4c3925183319b240442ebe8946bdb9011
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-11 23:26:06 +0530

    Main.java: Create a template for source code

---
+++ README.md
@@ -0,0 +1,8 @@
+# BCSC0002 - Object-Oriented Programming
+
+## Assignment 1
+
+### Date: 12 August 2019.
+### Deadline: 24 August 2019 till midnight.
+
+#### Instructions
---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * @author Divyansh Bhardwaj
+ * @version 1.0
+ * */
+package main;
+
+public class Main {
+	// TODO 1: Create the main method
+
+	/* TODO 2:
+	 *   a. Declare all your necessary variables inside main method.
+	 *   b. Declare a constant for 'gravity', assume the value to be 9.8
+	 *   c. Calculate the result and print it to the screen.
+	 * */
+
+	// P.S: Refer to the README.md file for the problem statement, input & output.
+}
