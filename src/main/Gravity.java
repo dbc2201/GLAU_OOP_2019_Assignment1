ab2cfcaaaacd29f84fec1b34e93450969cd56b0c
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-12 00:00:28 +0530

    Main.java: move TODO inside main() method

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -10,12 +10,11 @@ package main;
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
-
+		/* TODO 2:
+		 *   a. Declare all your necessary variables inside main method.
+		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
+		 *   c. Calculate the result and print it to the screen.
+		 * */
 	}
-	/* TODO 2:
-	 *   a. Declare all your necessary variables inside main method.
-	 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-	 *   c. Calculate the result and print it to the screen.
-	 * */
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
